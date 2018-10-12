426e31d37e60c9f0f07c93a0a0218c929a8d749c
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-10-12 08:49:06 -0700

    ajhsgd

--- broadbandspeed/settings.py
+++ broadbandspeed/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
